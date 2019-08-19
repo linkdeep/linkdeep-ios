c4a21db19da0e6d5a6eea4f7686663128ffb0df0
mike <mike.leong.kosun@gmail.com>
2019-08-19 15:21:03 +0800

    Add podspec

---
+++ LinkDeep.framework/Headers/LinkDeep.h
@@ -0,0 +1,19 @@
+//
+//  LinkDeep.h
+//  LinkDeep
+//
+//  Created by Mike on 19/06/2019.
+//  Copyright © 2019 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinkDeep.
+FOUNDATION_EXPORT double LinkDeepVersionNumber;
+
+//! Project version string for LinkDeep.
+FOUNDATION_EXPORT const unsigned char LinkDeepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkDeep/PublicHeader.h>
+
+#import <LinkDeep/LinkDeepManager.h>
